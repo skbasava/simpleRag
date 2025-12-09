887aa390a17c5132d7ff69f1a85b1ca02f3b8c2a
Satish Basavaraju <satish.aradhya@gmail.com>
2025-12-09 10:40:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+POSTGRES = {
+    "host": "localhost",
+    "port": 5432,
+    "db": "ragdb",
+    "user": "raguser",
+    "password": "ragpass"
+}
+
+WEAVIATE = {
+    "url": "http://localhost:8080",
+    "class": "AccessControlPolicy"
+}
+
+EMBED_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
